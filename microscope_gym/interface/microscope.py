e585114e3da75b46e1b6c42b2d2904f61fb008b5
thawn <webmaster@korten.at>
2023-03-28 09:43:03 +0200

    completed microscope interface

--- microscope_gym/interface/microscope.py
+++ microscope_gym/interface/microscope.py
@@ -29,6 +29,46 @@ class Microscope(ABC):
         pass
 
     @abstractmethod
+    def scan_stage_positions(self, y_range: tuple, x_range: tuple):
+        '''Scan stage positions in µm.
+
+        Args:
+            y_range (tuple of float):
+                range of y positions in µm, works like the range() function argument (start, stop, step).
+                If step is not given, it defaults to 90 % of the camera field of view height.
+                If only one argument is given, it is interpreted as the stop argument and start will be the minimum stage y range.
+                If y_range is None, the entire stage y range is used.
+                implemented by the _set_range() method.
+            x_range (tuple of float):
+                range of x positions in µm, works like the range() function argument (start, stop, step).
+                If step is not given, it defaults to 90 % of the camera field of view width.
+                If only one argument is given, it is interpreted as the stop argument and start will be the minimum stage x range.
+                If x_range is None, the entire stage x range is used.
+                implemented by the _set_range() method.
+        '''
+        pass
+
+    @abstractmethod
+    def get_stage_position(self) -> tuple:
+        '''Get stage position in µm.'''
+        pass
+
+    @abstractmethod
+    def get_sample_pixel_size_um(self) -> float:
+        '''Get pixel size in sample space in µm.'''
+        pass
+
+    @abstractmethod
+    def get_field_of_view_um(self) -> tuple:
+        '''Get field of view in µm.'''
+        pass
+
+    @abstractmethod
+    def get_metadata(self) -> dict:
+        '''Get metadata of microscope components and the corresponding pixel size and image dimensions in the sample.'''
+        pass
+
+    @abstractmethod
     def acquire_image(self) -> "numpy.ndarray":
         '''Acquire new image.'''
         pass
@@ -92,15 +132,14 @@ class Microscope(ABC):
         '''Acquire overview image.'''
         pass
 
-    @abstractmethod
-    def get_metadata(self) -> dict:
-        '''Get metadata of microscope components and the corresponding pixel size and image dimensions in the sample.'''
-        pass
-
-    @abstractmethod
-    def get_stage_position(self) -> tuple:
-        '''Get stage position in µm.'''
-        pass
+    def _set_range(self, range: tuple, default_range: tuple):
+        if len(range) < 1:
+            range = default_range
+        if len(range) < 2:
+            range = (default_range[0], range, default_range[2])
+        if len(range) < 3:
+            range = range + (default_range[2],)
+        return range
 
 
 # make stage position getter and setter
