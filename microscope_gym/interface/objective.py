b5b8dac8a9a2353a69b48130645e21c1ba1ee59a
thawn <webmaster@korten.at>
2023-04-09 11:34:20 +0200

    converted Objective into a pydantic data class

--- microscope_gym/interface/objective.py
+++ microscope_gym/interface/objective.py
@@ -1,24 +1,14 @@
 '''Microscope objective class.
 
 Since the objective does not have any methods, it is not implemented as an interface, but as a real class.'''
+from pydantic import BaseModel, Field
 
 
-class Objective():
-    '''Microscope objective class.
-
-    properties:
-        magnification: float
-            magnification factor
-        working_distance: float
-            working distance in µm
-        numerical_aperture: float
-            numerical aperture - NA = sin(θ) (θ = half angle of cone of light captured by objective)
-        immersion: str
-            immersion medium (e.g. "air", "oil", "water", "glycerol", "silicone oil")
-    '''
-
-    def __init__(self, magnification: float, working_distance: float, numerical_aperture: float, immersion: str):
-        self.magnification = magnification
-        self.working_distance = working_distance
-        self.numerical_aperture = numerical_aperture
-        self.immersion = immersion
+class Objective(BaseModel):
+    '''Microscope objectiveb data class.'''
+    name: str = Field(..., description="name of the objective")
+    magnification: float = Field(..., description="magnification factor")
+    working_distance: float = Field(..., description="working distance in mm")
+    numerical_aperture: float = Field(...,
+                                      description="numerical aperture - NA = sin(θ) (θ = half angle of cone of light captured by objective)")
+    immersion: str = Field(..., description="immersion medium (e.g. 'air', 'oil', 'water', 'glycerol', 'silicone oil')")
