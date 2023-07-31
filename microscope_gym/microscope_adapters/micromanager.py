88e1b7bac31e9a24e1aab580bb5f6193e9892bfa
hamdaan-shaikh <42545761+hamdaan-shaikh@users.noreply.github.com>
2023-07-31 16:59:26 +0200

    Added _update_axes_positions()

--- microscope_gym/microscope_adapters/micromanager.py
+++ microscope_gym/microscope_adapters/micromanager.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from typing import List
 from pycromanager import Core
 from microscope_gym import interface
 from microscope_gym.interface import Objective, Microscope, Axis
@@ -30,6 +31,22 @@ class Stage(interface.Stage):
 
         return self.microscope_handler.device_busy(
             focus_device_name) or self.microscope_handler.device_busy(stage_device_name)
+
+    def _update_axes_positions(self, axis_names: List[str], positions: List[float]):
+        '''Write new positions to axes.
+
+        Position validation is done in Axis model.
+
+        Parameters:
+            axis_names: list[str]
+                list of axis names
+            positions: list[float]
+                list of new positions (in um)
+        '''
+        for name, position in zip(axis_names, positions):
+            self.axes[name].position_um = position
+        self.microscope_handler.set_xy_stage_position(self.axes['x'].position_um, self.axes['y'].position_um)
+        self.microscope_handler.set_position(self.axes['z'].position_um)
 
 
 class Camera(interface.Camera):
