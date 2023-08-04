c728a229b1aca45fbb49a9c2128aa2e36d6a9e7d
Till Korten <webmaster@korten.at>
2023-08-04 17:45:02 +0200

    added version

--- microscope_gym/interface/__init__.py
+++ microscope_gym/interface/__init__.py
@@ -2,3 +2,5 @@ from microscope_gym.interface.camera import Camera, CameraSettings
 from microscope_gym.interface.stage import Stage, Axis
 from microscope_gym.interface.microscope import Microscope
 from microscope_gym.interface.objective import Objective
+
+__version__ = "0.0.1"
